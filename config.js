3ff6ffa5e75bfc9607e10b262e1df84d42df8634
Jungle <devex.soft@gmail.com>
2016-08-24 13:06:56 +0500

    formated config.js

--- config.js
+++ config.js
@@ -3,21 +3,21 @@ import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-  entry: 'src/index.js',
-  dest: 'dist/bundle.js',
-  format: 'iife',
-  banner: 'var global = this;',
-  plugins: [ 
-      buble(),
-      nodeResolve({
-		jsnext: true,
-	  	main: true,
-      }),
-      commonjs({
-		 include: 'node_modules/**',
-		 extensions: [ '.js', '.coffee'  ],  // Default: [ '.js'  ]
-		 ignoreGlobal: false,  // Default: false
-		 sourceMap: false,  // Default: true
-	  }),
-  ]
+    entry: 'src/index.js',
+    dest: 'dist/bundle.js',
+    format: 'iife',
+    banner: 'var global = this;',
+    plugins: [
+        buble(),
+        nodeResolve({
+            jsnext: true,
+            main: true,
+        }),
+        commonjs({
+            include: 'node_modules/**',
+            extensions: ['.js', '.coffee'], // Default: [ '.js'  ]
+            ignoreGlobal: false, // Default: false
+            sourceMap: false, // Default: true
+        }),
+    ]
 };
