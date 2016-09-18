c3bbda5ebe5840645adb6973dda48d6cd3b36b9f
Jungle <devex.soft@gmail.com>
2016-09-18 17:07:03 +0500

    fixed simple test end call

--- tests/index.js
+++ tests/index.js
@@ -7,18 +7,18 @@ export function doGet(event) {
 
     let p = new Promise((resolve, reject) => {
         test.onFinish(() => {
-            let result = ContentService.createTextOutput(s);
-            resolve(result);
+            resolve(ContentService.createTextOutput(s));
         });
     });
 
     test('simple test', (t) => {
         t.equal(1, 1);
+        t.end();
     });
 
     //return ContentService.createTextOutput('test handler');
     //return new Promise((resolve, reject) => {
-        //setTimeout(resolve, 1000, ContentService.createTextOutput('test promise'));
+    //setTimeout(resolve, 1000, ContentService.createTextOutput('test promise'));
     //})
     return p;
 }
