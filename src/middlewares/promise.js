1c5a7116b85a828c4cb18efd13dab19e6230a69e
Jungle <devex.soft@gmail.com>
2016-09-18 17:01:22 +0500

    fixed promise middleware

--- src/middlewares/promise.js
+++ src/middlewares/promise.js
@@ -2,9 +2,14 @@ export default function promiseMiddleware(type) {
     return (next) => (event) => {
         let result = next(event);
         if (result instanceof Promise) {
-            let value = {}
-            result.then((v) => value.valueOf = () => v);
-            return value;
+            let value = null;
+            let obj = {
+                valueOf() {
+                    return value;
+                }
+            }
+            result.then((v) => value = v);
+            return obj;
         }
         return result;
     }
