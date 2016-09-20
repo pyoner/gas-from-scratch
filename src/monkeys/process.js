7e6ef827b7ffc916fbe060cf5faf340dbe3c09b8
Jungle <devex.soft@gmail.com>
2016-09-20 13:58:14 +0500

    fixed process event monkey patch

--- src/monkeys/process.js
+++ src/monkeys/process.js
@@ -1,3 +1,20 @@
 import EventEmitter from 'events';
 
-EventEmitter.call(process);
+const funcs = [
+    'addListener',
+    'emit',
+    'setMaxListeners',
+    'on',
+    'once',
+    'removeListener',
+    'removeAllListeners',
+    'listeners',
+    'listenerCount',
+]
+
+let p = new EventEmitter();
+
+for (let i = 0; i < funcs.length; i++) {
+    let k = funcs[i];
+    process[k] = p[k].bind(p);
+}
