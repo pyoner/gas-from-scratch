50820e98d24548a94411d15ec780ecd69a633e10
Jungle <devex.soft@gmail.com>
2016-09-29 11:51:50 +0500

    added test for helper webAccessWrapper

---
+++ tests/local/helpers.js
@@ -0,0 +1,50 @@
+import test from 'tape';
+import { webAccessWrapper } from '../../src/helpers';
+
+test('webAccessWrapper', (t) => {
+    let success = () => true;
+    let failure = () => false;
+    let email = 'test@mail.com';
+
+    global.Session = {
+        getActiveUser() {
+            return {
+                getEmail() {
+                    return email;
+                }
+            }
+        }
+    }
+
+    t.test('test password', (t) => {
+        let password = '123';
+        let testPassword = webAccessWrapper({ password })(success, failure);
+
+        let result = testPassword({ parameter: { password } });
+        t.equal(result, true);
+
+        result = testPassword({ parameter: { password: 'bad password' } });
+        t.equal(result, false);
+        t.end();
+    });
+
+    t.test('test whitelist success', (t) => {
+        let whitelist = [email];
+        let testWhitelist = webAccessWrapper({ whitelist })(success, failure);
+
+        let result = testWhitelist({ parameter: {} });
+        t.equal(result, true);
+
+        t.end();
+    });
+
+    t.test('test whitelist failure', (t) => {
+        let whitelist = ['failure@mail.com'];
+        let testWhitelist = webAccessWrapper({ whitelist })(success, failure);
+
+        let result = testWhitelist({ parameter: {} });
+        t.equal(result, false);
+
+        t.end();
+    });
+});
