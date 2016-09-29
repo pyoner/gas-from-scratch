c81960645875db6973498ded58cd8a694d0e0284
Jungle <devex.soft@gmail.com>
2016-09-29 15:30:54 +0500

    added test for core.initApp with 0 and 2 middleware

--- tests/local/core.js
+++ tests/local/core.js
@@ -20,12 +20,29 @@ test('core initApp', (t) => {
         t.end();
     });
 
+    t.test('test with 0 middleware', (t) => {
+        let context = {};
+        initApp(app, [], context);
+
+        t.equal(context.doGet(1), 1);
+        t.end();
+    });
+
     t.test('test with 1 middleware', (t) => {
         let context = {};
         let middleware = (type) => (next) => (event) => 1 + next(event);
         initApp(app, [middleware], context);
 
         t.equal(context.doGet(1), 2);
+        t.end();
+    });
+
+    t.test('test with 2 middleware', (t) => {
+        let context = {};
+        let middleware = (type) => (next) => (event) => 1 + next(event);
+        initApp(app, [middleware, middleware], context);
+
+        t.equal(context.doGet(1), 3);
         t.end();
     });
 });
